7c72427382f44f9c0aaf4949dc76fbc8785a770a
Jochem kleine <jochemkleine@gmail.com>
2024-12-04 14:47:27 +0100

    Initial commit

---
+++ test/semitae.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Semitae from '../lib/semitae-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/semitae-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Semitae.SemitaeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
