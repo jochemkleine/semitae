01d73b3e32eb7a7417312a81353777d1086ec66f
Jochem kleine <jochemkleine@gmail.com>
2024-12-11 13:28:15 +0100

    working solution seperate asl

--- lib/semitae-stack.ts
+++ lib/semitae-stack.ts
@@ -1,25 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import * as appsync from 'aws-cdk-lib/aws-appsync';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
-import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Construct } from 'constructs';
+import * as fs from 'fs';
 import * as path from 'path';
 
 export class SemitaeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // Encounter Table
-    const encounterTable = new dynamodb.Table(this, 'EncounterTable', {
-      partitionKey: {
-        name: 'id',
-        type: dynamodb.AttributeType.STRING,
-      },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
-    });
 
     // Process Instruction Lambda
     const processInstruction = new lambda.Function(this, 'ProcessInstruction', {
@@ -29,15 +18,26 @@ export class SemitaeStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(30),
     });
 
-    // Create Step Function task
-    const processInstructionTask = new tasks.LambdaInvoke(this, 'ProcessInstructionTask', {
-      lambdaFunction: processInstruction,
-      resultPath: '$.taskResult',
+    // Generate Message Lambda
+    const generateMessage = new lambda.Function(this, 'GenerateMessage', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'generate_message.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas')),
+      timeout: cdk.Duration.seconds(30),
     });
+
+    // Load and prepare state machine definition
+    const definitionPath = path.join(__dirname, '../statemachine/definition.json');
+    const definitionTemplate = fs.readFileSync(definitionPath, 'utf-8');
+
+    // Replace the placeholders with actual Lambda ARNs
+    const definition = definitionTemplate
+      .replace('${ProcessInstructionFunctionArn}', processInstruction.functionArn)
+      .replace('${GenerateMessageFunctionArn}', generateMessage.functionArn);
 
     // Create the state machine
     const stateMachine = new sfn.StateMachine(this, 'InstructionStateMachine', {
-      definition: processInstructionTask,
+      definitionBody: sfn.DefinitionBody.fromString(definition),
       stateMachineType: sfn.StateMachineType.EXPRESS,
       timeout: cdk.Duration.minutes(5),
       tracingEnabled: true,
@@ -50,6 +50,10 @@ export class SemitaeStack extends cdk.Stack {
         includeExecutionData: true,
       },
     });
+
+    // Grant Lambda permissions
+    processInstruction.grantInvoke(stateMachine);
+    generateMessage.grantInvoke(stateMachine);
 
     // AppSync API
     const api = new appsync.GraphqlApi(this, 'SemitaeApi', {
@@ -64,9 +68,6 @@ export class SemitaeStack extends cdk.Stack {
         },
       },
     });
-
-    // DynamoDB as data source
-    const encounterTableDS = api.addDynamoDbDataSource('EncounterTableDS', encounterTable);
 
     // Step Functions as data source
     const sfnDs = api.addHttpDataSource(
@@ -84,32 +85,11 @@ export class SemitaeStack extends cdk.Stack {
     // Grant permission to execute state machine
     stateMachine.grantStartSyncExecution(sfnDs.grantPrincipal);
 
-    // Create Encounter Resolver
-    encounterTableDS.createResolver('CreateEncounter', {
-      typeName: 'Mutation',
-      fieldName: 'createEncounter',
-      requestMappingTemplate: appsync.MappingTemplate.fromString(`
-        {
-          "version": "2018-05-29",
-          "operation": "PutItem",
-          "key": {
-            "id": $util.dynamodb.toDynamoDBJson($util.autoId())
-          },
-          "attributeValues": {
-            "messageLog": $util.dynamodb.toDynamoDBJson([]),
-            "activePlayer": $util.dynamodb.toDynamoDBJson($context.arguments.player1Id),
-            "participants": $util.dynamodb.toDynamoDBJson([$context.arguments.player1Id, $context.arguments.player2Id]),
-            "createdAt": $util.dynamodb.toDynamoDBJson($util.time.nowISO8601())
-          }
-        }
-      `),
-      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem()
-    });
-
     // Send Instruction Resolver
     sfnDs.createResolver('SendInstruction', {
       typeName: 'Mutation',
       fieldName: 'sendInstruction',
+      // In the CDK stack, update the SendInstruction resolver's requestMappingTemplate:
       requestMappingTemplate: appsync.MappingTemplate.fromString(`
         {
           "version": "2018-05-29",
@@ -133,30 +113,11 @@ export class SemitaeStack extends cdk.Stack {
         #end
         #set($body = $util.parseJson($ctx.result.body))
         #set($output = $util.parseJson($body.output))
-        #if($output.taskResult)
-          #return($output.taskResult.Payload)
-        #end
         $util.toJson($output)
       `)
     });
 
-    // Get Encounter Resolver
-    encounterTableDS.createResolver('GetEncounter', {
-      typeName: 'Query',
-      fieldName: 'getEncounter',
-      requestMappingTemplate: appsync.MappingTemplate.fromString(`
-        {
-          "version": "2018-05-29",
-          "operation": "GetItem",
-          "key": {
-            "id": $util.dynamodb.toDynamoDBJson($context.arguments.id)
-          }
-        }
-      `),
-      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem()
-    });
-
-    // Output the API URL and API Key
+    // Outputs
     new cdk.CfnOutput(this, 'GraphQLApiUrl', {
       value: api.graphqlUrl
     });
